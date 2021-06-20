ee6c60159621bf236957a1ab9471e3c617a462e5
harini <harinisiva1997@gmail.com>
2021-06-20 18:58:23 +0530

    adding custom components and css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,6 @@
+import MeetUpDetail from '../../components/meetups/MeetupDetails'
+function MeetUpDetails() {
+    return <MeetUpDetail address='some add' title='first meetup' image='https://upload.wikimedia.org/wikipedia/commons/a/ad/Munnar_hillstation_kerala.jpg' description='some description' />
+}
+
+export default MeetUpDetails
