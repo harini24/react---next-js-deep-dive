7b705fdb13fd3e05b4bf35dac28ae7025453a8c2
harini <harinisiva1997@gmail.com>
2021-06-20 22:32:03 +0530

    working with params in SSG -- throws error

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,23 @@
 import MeetUpDetail from '../../components/meetups/MeetupDetails'
 function MeetUpDetails() {
-    return <MeetUpDetail address='some add' title='first meetup' image='https://upload.wikimedia.org/wikipedia/commons/a/ad/Munnar_hillstation_kerala.jpg' description='some description' />
+    return <MeetUpDetail
+        address='some add'
+        title='first meetup'
+        image='https://upload.wikimedia.org/wikipedia/commons/a/ad/Munnar_hillstation_kerala.jpg'
+        description='some description'
+    />
+}
+export async function getStaticProps() {
+    //fetch data api
+    return {
+        props: {
+            address='some add',
+            title='first meetup',
+            image='https://upload.wikimedia.org/wikipedia/commons/a/ad/Munnar_hillstation_kerala.jpg',
+            description='some description'
+        },
+        revalidate: 10
+    }
 }
 
 export default MeetUpDetails
