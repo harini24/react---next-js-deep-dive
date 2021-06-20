f70ff23a596e8f491695fa9874e3796770e7fc59
harini <harinisiva1997@gmail.com>
2021-06-20 18:20:31 +0530

    outputting a list of meetups

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import MeetupList from '../components/meetups/MeetupList'
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        titile: ' A first meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Munnar_Top_station.jpg/220px-Munnar_Top_station.jpg',
+        address: 'some address 5, some city abc'
+    },
+    {
+        id: 'm2',
+        titile: ' A second meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Munnar_Top_station.jpg/220px-Munnar_Top_station.jpg',
+        address: 'some address 5, some city abc'
+    }
+]
+function HomePage() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage
