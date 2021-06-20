b4feb3721d15f7fa1ce1642def74afc1d27311a8
harini <harinisiva1997@gmail.com>
2021-06-20 22:52:19 +0530

    preparing paths with setStaticPaths and working with fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,22 +1,45 @@
 import MeetUpDetail from '../../components/meetups/MeetupDetails'
-function MeetUpDetails() {
+function MeetUpDetails(props) {
     return <MeetUpDetail
-        address='some add'
-        title='first meetup'
-        image='https://upload.wikimedia.org/wikipedia/commons/a/ad/Munnar_hillstation_kerala.jpg'
-        description='some description'
+        address={props.address}
+        title={props.title}
+        image={props.image}
+        description={props.description}
     />
 }
-export async function getStaticProps() {
+export async function getStaticPaths() {
+
+    // generate dynamic
+    return {
+        fallback: true,// generate page dynamically for ids not in path, if false throws 404 page not found error for ids not specifies
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            }
+        ]
+    }
+}
+export async function getStaticProps(context) {
     //fetch data api
+
+    const meetupId = context.params.meetupId
+
+    console.log(meetupId)// founf in terminal and not in console
     return {
         props: {
-            address='some add',
-            title='first meetup',
-            image='https://upload.wikimedia.org/wikipedia/commons/a/ad/Munnar_hillstation_kerala.jpg',
-            description='some description'
-        },
-        revalidate: 10
+            id: meetupId,
+            address: 'some add',
+            title: 'first meetup',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Munnar_hillstation_kerala.jpg',
+            description: 'some description for meetup'
+        }
     }
 }
 
