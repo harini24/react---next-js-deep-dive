81975a0c139a81a632db8b051b198468a66b2d63
harini <harinisiva1997@gmail.com>
2021-06-20 22:06:58 +0530

    static site generation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import MeetupList from '../components/meetups/MeetupList'
 import Layout from '../components/layout/Layout'
+import { useEffect, useState } from 'react'
 
 const DUMMY_MEETUPS = [
     {
@@ -16,11 +17,29 @@ const DUMMY_MEETUPS = [
     }
 ]
 
-function HomePage() {
+function HomePage(props) {
+
+    // const [loadedMeetup, setLoadedMeetups] = useState([])
+
+    // useEffect(() => {
+    //     setLoadedMeetups(DUMMY_MEETUPS)
+    // }, []) not suitable for SOE
+
+
     // return <Layout>
     //     <MeetupList meetups={DUMMY_MEETUPS} />
     // </Layout>
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+    return <MeetupList meetups={props.meetups} />
+}
+
+export async function getStaticProps() {
+    //fetch data api
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        },
+        revalidate: 10
+    }
 }
 
 export default HomePage
